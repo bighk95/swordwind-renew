18d0d9c5ed4c95ea6cba3881740a2bb83a4e4530
현규 박 <wnsvmfh77@gmail.com>
2024-03-10 22:54:51 +0900

    feat: 적용된 패치 기능 구현중...

--- src/Controller.js
+++ src/Controller.js
@@ -1,31 +1,80 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styled from 'styled-components';
 
 const Controller = ({ onClose }) => {
+  const [targetId, setTargetId] = useState('');
+  const [targetPercentage, setTargetPercentage] = useState('');
+  const [renderAppliedId, setRenderAppliedId] = useState('');
+  const [renderAppliedPercentage, setRenderAppliedPercentage] = useState('');
+
+  const apply = () => {
+    setRenderAppliedId(targetId);
+    setRenderAppliedPercentage(targetPercentage);
+
+    setTargetId('');
+    setTargetPercentage('');
+
+    return (
+      <div className="appliedInfoContainer">
+        <div className="id">{renderAppliedId}</div>
+        <div className="percentage">{renderAppliedPercentage}</div>
+        <img src={require(`./img/exit.png`)} alt="deleteAppliedPatch"></img>
+      </div>
+    );
+  };
+
+  const handleInputId = (e) => {
+    const inputId = e.target.value;
+    setTargetId(inputId);
+  };
+
+  const handleInputPercentage = (e) => {
+    const inputPercentage = e.taget.value;
+    setTargetPercentage(inputPercentage);
+  };
+
   return (
     <ModalContainer onClick={onClose}>
       <Modal onClick={(e) => e.stopPropagation()}>
         <ModalTitle>유저 밸런스 패치</ModalTitle>
         <PatchContainer>
           <InputContainer>
-            <PlayerInput placeholder="소환사 이름 + #KR1"></PlayerInput>
-            <PercentageInput
-              type="number"
-              placeholder="배율 ex) 0.8 = 점수의 80%"
-            ></PercentageInput>
+            <FirstTitleContainer>
+              <Title1>이름</Title1>
+              <PlayerInput
+                placeholder="소환사 이름 + #KR1"
+                onChange={handleInputId}
+              ></PlayerInput>
+            </FirstTitleContainer>
+            <SecondTitleContainer>
+              <Title2>밸런스</Title2>
+              <PercentageInput
+                type="number"
+                placeholder="배율 ex) 0.8 = 점수의 80%"
+                onChange={handleInputPercentage}
+              ></PercentageInput>
+            </SecondTitleContainer>
           </InputContainer>
-          <ApplyContainer>
-            <Apply
-              onClick={(e) => {
-                e.stopPropagation();
-                alert('패치를 적용하였습니다.');
-              }}
-            >
-              적용
-            </Apply>
-          </ApplyContainer>
+          <Apply
+            onClick={(e) => {
+              e.stopPropagation();
+              apply();
+            }}
+          >
+            적용
+          </Apply>
         </PatchContainer>
-        <AppliedPatch></AppliedPatch>
+        <AppliedPatchContainer>
+          <AppliedPatch>적용된 패치</AppliedPatch>
+          <IndivController>
+            <AppliedId>홍길동</AppliedId>
+            <AppliedPercentage>0.8</AppliedPercentage>
+            <DeleteAppliedPatch
+              src={require(`./img/exit.png`)}
+              alt="deleteAppliedPatch"
+            ></DeleteAppliedPatch>
+          </IndivController>
+        </AppliedPatchContainer>
         <Reset
           src={require(`./img/reset.png`)}
           alt="reset"
@@ -63,9 +112,9 @@ const Modal = styled.div`
   background: white;
   padding: 20px;
   border-radius: 8px;
-  max-width: 500px;
+  max-width: 600px;
   width: 90%;
-  max-height: 500px;
+  max-height: 700px;
   height: 90%;
 
   position: relative;
@@ -89,22 +138,39 @@ const PatchContainer = styled.div`
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
-
+  margin-right: 30px;
   input::-webkit-inner-spin-button {
     appearance: none;
     -moz-appearance: none;
     -webkit-appearance: none;
   }
-  & > input {
-    width: 200px;
-    height: 36px;
-
-    margin: 10px;
-    font-size: 16px;
-  }
 `;
 
-const ApplyContainer = styled.div``;
+const Title1 = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+
+  margin-right: 20px;
+`;
+
+const Title2 = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+
+  margin-right: 20px;
+`;
+
+const FirstTitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SecondTitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Apply = styled.button`
   width: 100px;
@@ -120,9 +186,56 @@ const Apply = styled.button`
   font-weight: bold;
 `;
 
-const PlayerInput = styled.input``;
-const PercentageInput = styled.input``;
-const AppliedPatch = styled.div``;
+const PlayerInput = styled.input`
+  width: 200px;
+  height: 36px;
+
+  margin: 10px;
+  font-size: 16px;
+
+  border: 1px solid rgb(51, 51, 51);
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  outline: none;
+`;
+const PercentageInput = styled.input`
+  width: 200px;
+  height: 36px;
+
+  margin: 10px;
+  font-size: 16px;
+
+  border: 1px solid rgb(51, 51, 51);
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  outline: none;
+`;
+
+const AppliedPatchContainer = styled.div``;
+
+const AppliedPatch = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  margin: 50px 0 20px 50px;
+`;
+
+const IndivController = styled.div`
+  display: flex;
+
+  margin-left: 50px;
+`;
+
+const AppliedId = styled.div``;
+
+const AppliedPercentage = styled.div``;
+
+const DeleteAppliedPatch = styled.img`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+`;
 
 const Reset = styled.img`
   display: flex;
