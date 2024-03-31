4312c77d22a5ed5b109f8ee387374e9197aad7ac
현규 박 <wnsvmfh77@gmail.com>
2024-03-31 15:06:27 +0900

    배포설정

--- src/api/summoner.js
+++ src/api/summoner.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
+export const api = axios.create({
+  headers: {
+    'Cache-Control': 'no-cache',
+  },
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
+
 export const search = async (gameName, tagLine) => {
-  return await axios.get(
-    `/api/swordwind/summoner/search/${gameName}/${tagLine}`,
-  );
+  return await api.get(`/api/swordwind/summoner/search/${gameName}/${tagLine}`);
 };
 
 export const update = async (gameName, tagLine) => {
-  return await axios.post(
+  return await api.post(
     `/api/swordwind/summoner/update/${gameName}/${tagLine}`,
   );
 };
